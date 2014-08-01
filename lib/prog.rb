b4a9f4bc3bcf296ad24f19b6dca5de10a353eae6
patrick david <student@epicodus.com>
2014-08-01 16:52:59 -0700

    turns 10 to the string  'ten'

--- lib/prog.rb
+++ lib/prog.rb
@@ -1,13 +1,15 @@
 def number_text(number)
-  number_list = {"one" => 1}
+  number_list = { "hundred"=> 100, "ten"=> 10, "one" => 1}
   my_number = []
 
   number_list.each do |key, value|
     if number >= value
       my_number.push(key)
       number = number - value
+      redo
     end
   end
-  my_number = my_number.join
+
+  my_number = my_number.join(' ')
   my_number
 end
--- spec/prog_spec.rb
+++ spec/prog_spec.rb
@@ -5,4 +5,7 @@ describe 'number_text' do
   it 'will change the digit 1 to the string "one"' do
     expect(number_text(1)).to(eq('one'))
   end
+  it 'will change the digit 10 to the string "ten"' do
+    expect(number_text(10)).to(eq('ten'))
+  end
 end
